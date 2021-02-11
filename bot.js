56519f2f2d71caa35654c80ff55254fe3343b1f3
haduuuhh <77470911+haduuuhh@users.noreply.github.com>
2021-02-11 07:51:24 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -145,6 +145,37 @@ client.on("ready", () =>{
 
 
 
+module.exports = {
+    name: 'loop',
+    description: 'To loop song',
+    async execute(message, args, distube, wh, Prefix, MessageEmbed) {
+        message.delete()
+        let queue = await distube.getQueue(message)
+        if (queue) {
+            let mode = distube.setRepeatMode(message, parseInt(args[0]));
+            mode = mode ? mode == 2 ? "Repeat Semua Lagu" : "Repeat Lagu Ini" : "Mati";
+            wh.send({
+                username: 'haduuuressss Song Player',
+                avatarURL: 'https://i.imgur.com/pBmA5S6.png',
+                embeds: [new MessageEmbed()
+                    .setTitle('**Prefix:** `' + Prefix + '`')
+                    .setColor('#02C2FF')
+                    .setAuthor('haduuuressss', 'https://cdn.discordapp.com/app-icons/792994169659981846/eccf642340521c532b0ade8f00591114.png?size=64', 'https://minecraft-mp.com/server-s272254')
+                    .setDescription(':clipboard: **Diminta Oleh** <@' + message.author + '>\n\u200B\n\u200B')
+                    .addField(":repeat: **Mode Repeat Saat Ini** `" + mode + "`", '\u200B')
+                    .setFooter('haduuuhh', 'https://cdn.discordapp.com/avatars/700166055326384179/3ec8287199dc402fe6a587902e300749.png?size=64')]
+            })
+        } else if (!queue) {
+            wh.send(':x: **Bot tidak sedang memutar lagu!**', {
+                username: 'UniversNetwork Song Player',
+                avatarURL: 'https://i.imgur.com/pBmA5S6.png'
+            })
+        }
+    }
+}
+
+
+
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
